996c2e8f64513eb3daf80b055b0bb5e00af5d778
yuhasem <yuhasem@rose-hulman.edu>
2015-02-05 21:52:11 -0500

    fixed an improper image and made a preprocessor

--- preprocess.m
+++ preprocess.m
@@ -1,6 +1,28 @@
 function [ output_image ] = preprocess( image )
 
-output_image = rgb2gray(image);
+throughput_image = rgb2hsv(image);
+val = throughput_image(:,:,3);
+sat = throughput_image(:,:,2);
+val(val > .7) = 1;
+sat(val > .7) = 0;
+throughput_image(:,:,2) = sat;
+throughput_image(:,:,3) = val;
+output_image = hsv2rgb(throughput_image);
+output_image = rgb2gray(output_image);
+output_image(output_image < 1) = 0;
+output_image = 1 - output_image;
+[cc, n] = bwlabel(output_image);
+sizes = zeros(1, n);
+for i = 1:n
+    sizes(i) = length(find(cc == i));
+end
+med = median(sizes);
+for i = 1:n
+    if (sizes(i) < 0.35*med || sizes(i) > 3*med)
+        output_image(cc == i) = 0;
+    end
+end
+output_image = 1 - output_image;
 
 end
 
