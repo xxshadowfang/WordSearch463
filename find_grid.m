94af1dbe98d2ac0031cd2e32cb1bfdb202f7eb06
Dagwaging <dagwaging@gmail.com>
2015-02-12 22:41:10 -0500

    Add spacing and offset calculation using k-means

--- find_grid.m
+++ find_grid.m
@@ -6,17 +6,17 @@ cc = bwconncomp(1-image, 8);
 % Take bounding boxes
 boxes = regionprops(cc, 'BoundingBox');
 
-points = zeros(size(boxes, 1), 2);
+initialPoints = zeros(size(boxes, 1), 2);
 
 for i = 1:size(boxes, 1)
     area = boxes(i).BoundingBox(3) * boxes(i).BoundingBox(4);
     center = [boxes(i).BoundingBox(1) + boxes(i).BoundingBox(3) / 2, boxes(i).BoundingBox(2) + boxes(i).BoundingBox(4) / 2];
     
     % Take their centroids
-    points(i, :) = center;
+    initialPoints(i, :) = center;
 end
 
-points = repmat(points, 1, 1, size(boxes, 1));
+points = repmat(initialPoints, 1, 1, size(boxes, 1));
 
 points_temp = permute(points, [3 2 1]);
 
@@ -36,9 +36,41 @@ for i = 1:size(angles,1)
 end
 angles = newang(:, 2:4);
 
-scatter(distances(:), angles(:));
+[x, y] = pol2cart(angles(:), distances(:));
 
-% Perform k-means on distance + angle pairs with 2 means
+% hold on;
+% scatter(x(:), y(:));
+
+
+offsets = [x(:) y(:)];
+
+[index, centers] = kmeans(offsets, 2);
+[theta, rho] = cart2pol(centers(:, 1), centers(:, 2));
+
+% viscircles(centers, [2; 2]);
+
+pointsModX = initialPoints * [cos(-theta(1)) -sin(-theta(1)); sin(-theta(1)) cos(-theta(1))];
+pointsModY = initialPoints * [cos(-theta(2)) -sin(-theta(2)); sin(-theta(2)) cos(-theta(2))];
+
+centers
+
+offsetX = median(mod(pointsModX(:, 1), rho(1)))
+offsetY = median(mod(pointsModY(:, 1), rho(2)))
+
+imshow(image);
+
+circles = zeros(40 * 40, 2);
+
+for i=1:40
+    for j=1:40
+        x = centers(1, :) .* (i - 20) + centers(1, :) ./ norm(centers(1, :)) .* offsetX;
+        y = centers(2, :) .* (j - 20) + centers(2, :) ./ norm(centers(2, :)) .* offsetY;
+        
+        circles((i - 1) * 40 + j, :) = x + y;
+    end
+end
+
+viscircles(circles, repmat([10], 40 * 40, 1), 'LineWidth', 1);
 
 %When we find the angle, we should rotate the image, then find the offsets
 
